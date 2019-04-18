ddf518337cccc452d36f1844c42041493b558ddb
codebug8 <livelwh@outlook.com>
2019-04-18 11:58:34 +0800

    add Compiler_uboot.sh shell
    Signed-off-by: codebug8 <livelwh@outlook.com>

--- Compiler_uboot.sh
+++ Compiler_uboot.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-
-export PATH=$PATH:../ToolChain/gcc-linaro-6.2.1-2016.11-x86_64_arm-linux-gnueabihf/bin
+CPU=$(cat /proc/cpuinfo |grep 'processor' |wc -l)
+GET=$(dirname $(readlink -f 'pwd'))
+export PATH=$PATH:${GET%uboot2016.06}/ToolChain/gcc-linaro-6.2.1-2016.11-x86_64_arm-linux-gnueabihf/bin/
+echo $PATH
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
 
@@ -14,8 +15,11 @@ make 100ask_am335x_defconfig
 
 
 #step 3:compiler uboot
-make 
 
+make -j$CPU
+
+echo -e "\033[31m ----------------------- \033[0m"
 ls -la MLO u-boot.img
 
+echo -e "\033[31m ----------------------- \033[0m"
 
